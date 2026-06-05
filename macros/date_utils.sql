724032e92d1d7a80ef83ffba133972cb6d50e0a2
rjsgamboa@gmail.com <rjsgamboa@gmail.com>
2026-06-05 15:36:25 +0000

    added date_utils

---
+++ macros/date_utils.sql
@@ -0,0 +1,20 @@
+(% macro function_past(x) %)
+
+CASE WHEN TO_TIMESTAMP({x})) < CURRENT_DATE THEN 'PAST'
+ELSE 'FUTURE' END
+
+(% endmacro %)
+
+(% macro get_season(x) %)
+
+
+CASE WHEN MONTH(TO_TIMESTAMP({x})) in (12,1,2)
+THEN 'WINTER'
+WHEN MONTH(TO_TIMESTAMP({x})) in (3,4,5)
+THEN 'SPRING'
+WHEN MONTH(TO_TIMESTAMP({x})) in (6,7,8)
+THEN 'SUMMER'
+ELSE 'AUTUMN'
+ END 
+
+(% endmacro %)
